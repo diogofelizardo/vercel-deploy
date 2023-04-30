c918f0f0a153a046888370b35409d8e70d369288
diogofelizardo <diogo.felizardo@hotmail.com>
2023-04-30 10:22:04 -0300

    init project

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express, { Router } from 'express'
+
+const app = express()
+
+const route = Router()
+
+route.get('/', (req, res) => {
+    return res.json({
+        success: true,
+        message: "Sucesso!"
+    })
+})
+
+app.use(route)
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log('Server running in ' + port)
+})
